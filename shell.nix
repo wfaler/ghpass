e808d38fbe905b5e36c88882d1de22fece00c31a
Wille Faler <wille.faler@gmail.com>
2015-07-22 22:37:44 +0000

    initial setup

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{ nixpkgs ? import <nixpkgs> {},haskellPackages ? (import <nixpkgs> {}).haskellPackages, compiler ? "ghc7101" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, base, bytestring, either
+      , monad-control, mtl, random, stdenv, text, time, transformers
+      , uuid
+      }:
+      mkDerivation {
+        pname = "ghpass";
+        version = "0.1.0.0";
+        src = ./src;
+        isLibrary = false;
+        isExecutable = true;
+        buildTools = [ haskellPackages.cabal-install haskellPackages.happy haskellPackages.alex haskellPackages.hlint];
+        buildDepends = [
+          aeson base bytestring either monad-control mtl random text time
+          transformers uuid
+        ];
+        homepage = "https://github.com/wfaler/ghpass";
+        license = stdenv.lib.licenses.unfree;
+      };
+
+  drv = pkgs.haskell.packages.${compiler}.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
