f901833c46da2f32056d8f932616d2d4a0086b07
Wille Faler <wille.faler@gmail.com>
2015-07-26 17:03:31 +0000

    WIP

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,27 @@
 {-# LANGUAGE OverloadedStrings,DeriveGeneric #-}
-{-# LANGUAGE ExtendedDefaultRules #-}
+
 import Shelly
-import Data.Text as T
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
 import System.IO
 import Control.Exception
 import GHC.Generics
 import Data.Aeson
-default (T.Text)
-
+import qualified System.Process as P
+import qualified System.Info as INFO
+import qualified Data.Text.Lazy.Encoding as TE
+import qualified System.Directory as DIR
+import System.Exit
+import System.Environment
+import System.Random
+import qualified Data.List as L
+import Data.UUID.V4
+import qualified Data.UUID as UUID
 
 type Passphrase = T.Text
 type Contents = T.Text
 type Filename = T.Text
+
 
 data Index = Index {entries :: [IndexEntry]} deriving(Show,Eq,Generic)
 data IndexEntry = IndexEntry { label :: T.Text, file :: T.Text} deriving(Show,Eq,Generic)
@@ -32,24 +42,84 @@ instance ToJSON Index
 
 main :: IO ()
 main = do
-  putStrLn $ "hello "
+  args <- getArgs
+  processArgs args
+  where
+    processArgs ["init"] = initStore
+    processArgs ["gen", "--no-specials", pwdLength] = do
+      _ <- genPass genPwdNoSpecials pwdLength
+      putStrLn "A new password has ben generated for you and put in your clipboard"
+    processArgs ["gen", pwdLength] = do
+      _ <- genPass genPwdSpecials pwdLength
+      putStrLn "A new password has ben generated for you and put in your clipboard"
+    processArgs ["new", entry, username, pwdLength] = do
+      pass <- promptPass
+      idx <- index pass
+      passwd <- (genPass genPwdSpecials pwdLength)
+      newEntry entry username passwd pass idx
+    processArgs ["new", "--no-specials", entry, username, pwdLength] = do
+      pass <- promptPass
+      idx <- index pass
+      passwd <- (genPass genPwdNoSpecials pwdLength)
+      newEntry entry username passwd pass idx
+    processArgs ["clip", entry] = error "todo"
+    processArgs ["show", entry] = error "todo"
+    processArgs ["search", substr] = error "todo"
+    processArgs ["import1password", fileName] = error "todo"
+    processArgs ["update", entry, key, value] = error "todo"
+    processArgs ["rm", entry] = error "todo"
+    processArgs ["rm-key", entry, key] = error "todo"
+    processArgs __ = do
+      putStrLn "your options did not match any valid options"
+      exitFailure
 
+newEntry entry username newPwd pass idx = do
+  entryExists <- return $ any (\x -> (label x) == (T.pack entry)) (entries idx)
+  if (entryExists)
+    then
+    do
+      putStrLn $ "entry with name " ++ entry ++ "already exists!"
+      exitFailure
+    else
+    do
+      filename <- nextRandom
+      dtDir <- dataDir
+      entry <- return $ Entry [(KeyValue "username" (T.pack username)), (KeyValue "password" newPwd)]
+      putStrLn "do stuff"
 
--- TODO: THIS DOESN'T WORK YET!
-clip :: T.Text -> IO T.Text
-clip text = do
-  shelly $ silently $ do
-    cmd "echo" [ T.concat ["echo -n ", text, " | xclip -selection clipboard"]]
+genPass :: Read a => (a -> [t] -> IO T.Text) -> String -> IO T.Text
+genPass genFn pwdLength = do
+      passwd <- genFn (read pwdLength) []
+      _ <- clip passwd
+      return passwd
+
+clip :: T.Text -> IO P.ProcessHandle
+clip = do
+  if (INFO.os == "linux")
+    then xclip
+    else pbcopy
+
+gpg :: Shelly.FilePath
+gpg = "gpg2"
+  
+pbcopy :: T.Text -> IO P.ProcessHandle
+pbcopy text = P.runCommand $ "echo -n '" ++ (T.unpack text) ++ "' | pbcopy"
+
+xclip :: T.Text -> IO P.ProcessHandle
+xclip text = P.runCommand $ "echo -n '" ++ (T.unpack text) ++ "' | xclip -selection clipboard"
+
+encrypt :: Contents -> Filename -> Passphrase -> IO T.Text
+encrypt = encryptAES256
 
 encryptAES256 :: Contents -> Filename -> Passphrase -> IO T.Text
 encryptAES256 contents filename passphrase = do
   shelly $ silently $ do
-    (-|-) (run "echo" ["-n", contents]) (run "gpg2" ["-ac", "-o", filename, "--cipher-algo", "AES256", "--batch", "--yes", "--passphrase", passphrase])
+    (-|-) (run "echo" ["-n", contents]) (run gpg ["-ac", "-o", filename, "--cipher-algo", "AES256", "--batch", "--yes", "--passphrase", passphrase])
 
-decryptAES256 :: Filename -> Passphrase -> IO T.Text
-decryptAES256 filename passphrase = do
+decrypt :: Filename -> Passphrase -> IO T.Text
+decrypt filename passphrase = do
   shelly $ silently $ do
-    (run "gpg2" ["--decrypt", "--cipher-algo", "AES256", "--batch", "--yes", "--passphrase", passphrase, filename])
+    (run gpg ["--decrypt", "--batch", "--yes", "--passphrase", passphrase, filename])
 
 
 promptPass :: IO Passphrase
@@ -71,3 +141,90 @@ withEcho :: Bool -> IO a -> IO a
 withEcho e action = do
   old <- hGetEcho stdin
   bracket_ (hSetEcho stdin e) (hSetEcho stdin old) action
+
+fromTxt :: FromJSON a => T.Text -> Maybe a
+fromTxt text = (decode . TE.encodeUtf8 . TL.fromChunks) [text]
+
+toTxt :: ToJSON a => a -> T.Text
+toTxt jsonObj = T.concat ((TL.toChunks . TE.decodeUtf8 . encode) jsonObj)
+
+fromFile :: FromJSON a => Filename -> Passphrase -> IO a
+fromFile pth pass = do
+  contents <- decrypt pth pass
+  validFile $ fromTxt contents
+  where
+    validFile (Just a) = return a
+    validFile (Nothing) = do
+      putStrLn ("file " ++ (show pth) ++ "does not exist!")
+      exitFailure
+  
+
+passDir :: IO System.IO.FilePath
+passDir = do
+  homeDir <- DIR.getHomeDirectory
+  return $ homeDir ++ "/.ghpass/"
+
+dataDir :: IO System.IO.FilePath
+dataDir = do
+  dir <- passDir
+  return $ dir ++ "data/"
+
+initStore :: IO ()
+initStore = do
+  dir <- passDir
+  exists <- DIR.doesDirectoryExist dir
+  if exists
+    then do
+    putStrLn $ dir ++ " already exists!"
+    exitFailure
+    else do
+    passphrase <- setPassphrase
+    initIndex passphrase dir
+    where
+      initIndex Nothing _ = do
+        putStrLn "Passphrases do not match, exiting"
+        exitFailure
+      initIndex (Just pass) dir = do
+        DIR.createDirectory dir
+        dataDr <- dataDir
+        DIR.createDirectory dataDr
+        _ <- saveFile (Index []) (dir ++ "index.gpg") pass
+        putStrLn "Initialised new ghpass store"
+
+
+nums :: [Char]
+nums = ['0'..'9']
+
+lowers :: [Char]
+lowers = ['a'..'z']
+
+uppers :: [Char]
+uppers = ['A'..'Z']
+
+specials :: [Char]
+specials = ['!', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '[', ']', '{', '}', ':', ';', '@',  '~', '#', '.', '?' ]
+
+genPwdNoSpecials :: Int -> [Char] -> IO T.Text
+genPwdNoSpecials = genPwd (nums ++ lowers ++ uppers)
+genPwdSpecials :: Int -> [Char] -> IO T.Text
+genPwdSpecials = genPwd (nums ++ lowers ++ uppers ++ specials)
+
+genPwd :: [Char] -> Int -> [Char] -> IO T.Text
+genPwd allChars len list = do
+  maxIndex <- return ((length allChars) - 1)
+  number <- getStdRandom (randomR (0,maxIndex))
+  newList <- return $ (allChars !! number) : list
+  if((length newList) < len)
+    then genPwd allChars len newList
+    else return $ T.pack newList
+    
+index :: Passphrase -> IO Index
+index pass = do
+  dr <- passDir
+  fromFile (T.pack (dr ++ "index.gpg")) pass :: IO Index
+  
+uuidToText :: UUID.UUID -> T.Text
+uuidToText = T.pack . UUID.toString
+
+saveFile :: ToJSON a => a -> String -> Passphrase -> IO T.Text
+saveFile obj file pass = encrypt (toTxt $ obj) (T.pack file) pass
